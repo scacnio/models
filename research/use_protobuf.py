d9bfa69a1a0c526d7aaafce2c419bdd602e78d2b
Samuel Cacnio <scacnio@sandiego.edu>
2023-04-23 14:21:28 -0700

    transferred dice detection files

---
+++ research/use_protobuf.py
@@ -0,0 +1,9 @@
+#Credit to BenGreenfield825: https://github.com/BenGreenfield825
+import os
+import sys
+args = sys.argv
+directory = args[1]
+protoc_path = args[2]
+for file in os.listdir(directory):
+    if file.endswith(".proto"):
+        os.system(protoc_path+" "+directory+"/"+file+" --python_out=.")
