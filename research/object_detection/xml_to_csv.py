e7133f15d2a3a13f90d4854026155311c65067c3
Samuel Cacnio <scacnio@sandiego.edu>
2023-05-01 14:09:01 -0700

    new csv records

--- research/object_detection/xml_to_csv.py
+++ research/object_detection/xml_to_csv.py
@@ -30,9 +30,9 @@ def xml_to_csv(path):
 
 def main():
     for folder in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), ('dice/' + folder))
+        image_path = os.path.join(os.getcwd(), ('DICE/' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('dice/'+folder+'_labels.csv'), index=None)
+        xml_df.to_csv(('DICE/'+folder+'_labels.csv'), index=None)
     print('Successfully converted xml to csv.')
 
 
