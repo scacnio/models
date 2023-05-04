1eec448f382b642b58ec6adafd9ef2dfe928d80c
Samuel Cacnio <scacnio@sandiego.edu>
2023-05-03 22:47:21 -0700

    wut model

--- research/object_detection/exporter_main_v2.py
+++ research/object_detection/exporter_main_v2.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# HNRS command: python exporter_main_v2.py --trained_checkpoint_dir=HNRS_training --pipeline_config_path=HNRS_ROOT.config --output_directory=HNRS_inference
+# HNRS command: python exporter_main_v2.py --trained_checkpoint_dir=HNRS_training --pipeline_config_path=ssd_efficientdet_d4_1024x1024_coco17_tpu-32.config --output_directory=HNRS_inference
 
 r"""Tool to export an object detection model for inference.
 
