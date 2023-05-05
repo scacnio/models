030cc7dbfdd066239c4339ff307a11a064a39d26
Samuel Cacnio <scacnio@sandiego.edu>
2023-05-05 09:56:54 -0700

    mods

--- research/object_detection/model_downloader.py
+++ research/object_detection/model_downloader.py
@@ -1,8 +1,8 @@
 #copied from https://github.com/BenGreenfield825
 import wget
-model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz"
+model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8.tar.gz"
 wget.download(model_link)
 import tarfile
-tar = tarfile.open('efficientdet_d0_coco17_tpu-32.tar.gz')
+tar = tarfile.open('ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8.tar.gz')
 tar.extractall('.')
 tar.close()
